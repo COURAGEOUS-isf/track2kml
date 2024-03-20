bde9498d4b37443f7cf6ab7c73798089ec8456d1
aleokdev <aleok.inf@gmail.com>
2024-03-20 09:32:55 +0100

    Fix release generation script

--- generate_release.sh
+++ generate_release.sh
@@ -1,12 +1,13 @@
-rm -r release
-mkdir release
+rm -r release/track2kml
+mkdir -p release/track2kml 2> /dev/null
 
-cp cli/README.md release
+cp cli/README.md release/track2kml/
 
-cargo build --target x86_64-unknown-linux-musl --release && mv target/x86_64-unknown-linux-musl/release/track2kml release/track2kml
-cargo build --release --target x86_64-pc-windows-gnu && mv target/x86_64-pc-windows-gnu/release/track2kml.exe release/track2kml.exe
+cargo build -p track2kml-cli --target x86_64-unknown-linux-musl --release && mv target/x86_64-unknown-linux-musl/release/track2kml release/track2kml/track2kml
+cargo build -p track2kml-cli --release --target x86_64-pc-windows-gnu && mv target/x86_64-pc-windows-gnu/release/track2kml.exe release/track2kml/track2kml.exe
 
 
-version=$(release/COURAGEOUS/track2kml --version | grep -oE "[^ ]+$")
+version=$(release/track2kml/track2kml --version | grep -oE "[^ ]+$")
 
-zip -ur "release/COURAGEOUS-${version}.zip" release
+cd release
+zip -urq "${version}.zip" track2kml
